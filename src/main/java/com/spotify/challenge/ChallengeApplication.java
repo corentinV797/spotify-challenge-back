efd0091889be31477d38a3b2d2fff60d21f77b34
Corentin VIVES <c.vives@groupeonepoint.com>
2022-02-10 16:23:33 +0100

    [feat] ChallengeApplication: enable cors

--- src/main/java/com/spotify/challenge/ChallengeApplication.java
+++ src/main/java/com/spotify/challenge/ChallengeApplication.java
@@ -2,6 +2,10 @@ package com.spotify.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ChallengeApplication {
@@ -10,4 +14,13 @@ public class ChallengeApplication {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/albums/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
